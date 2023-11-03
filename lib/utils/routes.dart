d63ac03c3e4bf556c1972da5da5668505767c577
Sadiya <sadiyaayub16@gmail.com>
2023-11-03 12:37:02 +0530

    welcome update

---
+++ lib/utils/routes.dart
@@ -0,0 +1,34 @@
+import 'package:finkin_credential/pages/otp_page.dart';
+import 'package:finkin_credential/welcom_carousel.dart/page1.dart';
+import 'package:finkin_credential/welcom_carousel.dart/page2.dart';
+import 'package:finkin_credential/welcom_carousel.dart/page3.dart';
+import 'package:finkin_credential/welcom_carousel.dart/welcome.dart';
+import 'package:flutter/material.dart';
+import '../pages/gen_OTP.dart';
+
+class MyRoutes {
+  static String genotpRoute = "/genotp";
+  static String OtpPageRoute = "/OtpPage";
+  static String page1 = "/page1";
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+    switch (settings.name) {
+      case '/genotp':
+        return MaterialPageRoute(builder: (context) => const Genotp());
+        case '/page1':
+        return MaterialPageRoute(builder: (context) => const Page1());
+      
+      case '/OtpPage':
+       return MaterialPageRoute(builder: (context) => const OtpPage());
+                
+  case '/page2':
+       return MaterialPageRoute(builder: (context) =>  const Page2());
+       
+       case '/page3':
+       return MaterialPageRoute(builder: (context) => const Page3());
+      default:
+        return MaterialPageRoute(builder: (context) =>  WelcomePage());
+    }
+  }
+}
---
+++ lib/welcom_carousel.dart/welcome.dart
@@ -0,0 +1,49 @@
+
+import 'package:finkin_credential/welcom_carousel.dart/page1.dart';
+import 'package:finkin_credential/welcom_carousel.dart/page2.dart';
+import 'package:finkin_credential/welcom_carousel.dart/page3.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class WelcomePage extends StatelessWidget {
+  final _controller = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[200],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          // page view
+          SizedBox(
+            height: 500,
+            child: PageView(
+              controller: _controller,
+              children: const [
+                Page1(),
+                Page2(),
+                Page3(),
+              ],
+            ),
+          ),
+
+          // dot indicators
+          SmoothPageIndicator(
+            controller: _controller,
+            count: 3,
+            effect: JumpingDotEffect(
+              activeDotColor: Colors.deepPurple,
+              dotColor: Colors.deepPurple.shade100,
+              dotHeight: 15,
+              dotWidth: 15,
+              spacing: 16,
+              //verticalOffset: 50,
+              jumpScale: 3,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
